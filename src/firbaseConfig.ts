6cf5df632e50d871da7fe97ff31c712c11b9b660
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2022-04-05 12:57:03 +0530

    Update firbaseConfig.ts
    config removed

--- src/firbaseConfig.ts
+++ src/firbaseConfig.ts
@@ -6,14 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWtWgOnJvfdK1hhL2FvO01tfCLaJNR4cg",
-  authDomain: "fir-crud-typescript.firebaseapp.com",
-  databaseURL:
-    "https://fir-crud-typescript-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fir-crud-typescript",
-  storageBucket: "fir-crud-typescript.appspot.com",
-  messagingSenderId: "548949882488",
-  appId: "1:548949882488:web:659643de006538b48a1f8d",
+  
 };
 
 // Initialize Firebase
